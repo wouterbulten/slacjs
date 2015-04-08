94323135682c7a1c2d048b97f05e7376dfd97469
Wouter Bulten <wouterbulten@mai-ai.org>
2015-04-08 13:25:39 +0200

    Small bug in user motion

--- app/models/user.js
+++ app/models/user.js
@@ -3,7 +3,7 @@ var User = function(x, y, xMax, yMax) {
 	//Current position
 	this.x = x;
 	this.y = y;
-	this.r = MathAdapter.randn(0,1) * 2 * Math.PI;
+	this.r = Math.random() * 2 * Math.PI;
 
 	//Current velocity
 	this.v = 5;
@@ -31,16 +31,17 @@ User.prototype.step = function() {
 	this.dx = Math.cos(this.r) * this.v;
 	this.dy = Math.sin(this.r) * this.v;
 
-	xn = Math.max(Math.min(this.x + this.dx + MathAdapter.randn(0,2), this.xMax), 0);
-	yn = Math.max(Math.min(this.y + this.dy + MathAdapter.randn(0,2), this.yMax), 0);
+	xn = Math.max(Math.min(this.x + this.dx + MathAdapter.randn(0,1), this.xMax), 0);
+	yn = Math.max(Math.min(this.y + this.dy + MathAdapter.randn(0,1), this.yMax), 0);
 	
 	if(xn == 0 || xn == this.xMax) {
-		this.dr = Math.PI - this.r;
+		this.r = Math.PI - this.r;
 	}
 	else if(yn == 0 || yn == this.yMax) {
-		this.dr = 2 * Math.PI - this.r;
+		this.r = 2 * Math.PI - this.r;
 	}
-	this.r = this.dr //+ MathAdapter.randn(0,1);
+
+	this.dr = this.r //+ MathAdapter.randn(0,1);
 	
 	this.moveToPosition(xn, yn)
 };
