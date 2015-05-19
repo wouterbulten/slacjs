d00deb2e592b4e62f647c7d26d039f5690d8befb
Wouter Bulten <wouterbulten@mai-ai.org>
2015-05-19 20:11:35 +0200

    Added test task for travis

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ function getTask(task) {
 
 gulp.task('lint', getTask('lint'));
 gulp.task('clean', getTask('clean'));
+gulp.task('test', ['lint']); //Used by Travis
 
 //@todo Sometimes html build task breaks style build task
 gulp.task('build', ['clean'], function() {
@@ -26,47 +27,3 @@ gulp.task('build-js', getTask('transpile'));
 gulp.task('build-styles', getTask('styles'));
 gulp.task('build-vendor', getTask('vendor'));
 gulp.task('build-polyfill', getTask('polyfill'));
-
-
-// /*
-// Reload browser
-//  */
-// gulp.task('reload-styles', ['styles'], browserSync.reload);
-// gulp.task('reload-scripts', ['lint', 'modules'], browserSync.reload);
-// gulp.task('reload-index', ['index'], browserSync.reload);
-
-// /*
-// Default task running all sub task in the right order
-//  */
-// gulp.task('default', ['clean'], function() {
-// 	gulp.start('lint', 'modules', 'vendor', 'polyfill', 'styles', 'index');
-// });
-
-// /*
-// Test task used by Travis
-//  */
-// gulp.task('test', function() {
-// 	return gulp.src(scripts)
-// 		.pipe(cached(scripts))
-// 		.pipe(jshint())
-// 		.pipe(jscs())
-// });
-
-// /*
-// Watch for changes and rebuild when necessary
-//  */
-// gulp.task('serve', ['default'], function() {
-
-// 	// Watch .css files
-// 	gulp.watch(styles, ['reload-styles']);
-
-// 	// Watch .js files
-// 	gulp.watch(scripts, ['reload-scripts']);
-// 	gulp.watch(tests, ['reload-scripts']);
-	
-// 	gulp.watch(index, ['reload-index']);
-
-// 	browserSync({
-// 		server: "./dist"
-// 	});
-// });
