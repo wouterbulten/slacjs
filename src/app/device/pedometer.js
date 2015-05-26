ecafb6a435a14743897a3c4fa9bc50e14f1380c9
Wouter Bulten <wouterbulten@mai-ai.org>
2015-05-26 13:56:03 +0200

    Added pedometer.

---
+++ src/app/device/pedometer.js
@@ -0,0 +1,164 @@
+/**
+ * Accelerometer based pedometer
+ *
+ * Based on a FirefoxOS ES5 implementation.
+ *
+ * @see http://sebastien.menigot.free.fr/index.php?view=article&id=93
+ */
+class Pedometer {
+
+	constructor(updateRate) {
+
+		const windowSize = Math.round(2 / (updateRate / 1000));
+
+		this.accNorm = new Array(windowSize); // amplitude of the acceleration
+
+		this.varAcc   = 0.0; // variance of the acceleration on the window L
+		this.minAcc   = 1.0;  // minimum of the acceleration on the window L
+		this.maxAcc   = -Infinity; // maximum of the acceleration on the window L
+		this.threshold = -Infinity; // threshold to detect a step
+		this.sensibility = 1.0 / 30.0;  // sensibility to detect a step
+
+		this.countStep = 0;           // number of steps
+		this.stepArr   = new Array(windowSize); // steps in 2 seconds
+
+		this.speed     = 0;  // instantaneous speed of the pedestrian (m/s)
+		this.meanSpeed = 0;  // mean speed of the pedestrian (m/s)
+
+		this.updateRate = updateRate; //Update rate in ms
+
+		this.filter = new KalmanFilter();
+	}
+
+	processMeasurement(x, y, z) {
+
+		const norm = this._computeNorm(x, y, z);
+		this.accNorm.push(norm);
+		this.accNorm.shift();
+
+		this._stepDetection();
+	}
+
+	_stepDetection() {
+
+		this._computeAccelerationVariance();
+		this.minAcc = Math.min.apply(null, this.accNorm);
+		this.maxAcc = Math.max.apply(null, this.accNorm);
+
+		this.threshold = (this.minAcc + this.maxAcc) / 2;
+
+		const diff = this.maxAcc - this.minAcc;
+
+		if (
+
+			//Sensiblity, the difference must increase the sensibility
+			Math.abs(diff) >= this.sensibility &&
+
+			//Acceleration must be above the threshold, and the previous one below (i.e. a new step)
+			(this.accNorm[this.accNorm - 1] >= this.threshold) && (this.accNorm[this.accNorm.length - 2] < this.threshold) &&
+
+			(this.stepArr[this.stepArr.length - 1] === 0)
+		) {
+			this.countStep++;
+			this.stepArr.push(1);
+			this.stepArr.shift();
+		}
+		else {
+			this.stepArr.push(0);
+			this.stepArr.shift();
+		}
+	}
+
+	/**
+	 * Compute the norm of the acceleration vector
+	 * @param  {Number} x
+	 * @param  {Number} y
+	 * @param  {Number} z
+	 * @return {Number} norm of the vector
+	 */
+	_computeNorm(x, y, z) {
+		const norm = Math.sqrt((x * x) + (y * y) + (z * z));
+		const filteredNorm = this.filter.filter(norm);
+
+		return filteredNorm / 9.80665;
+	}
+
+	/**
+	 * Compute the variance of the acceleration norm vector
+	 * @return {void}
+	 */
+	_computeAccelerationVariance() {
+		let mean  = 0.0;
+		let mean2 = 0.0;
+
+		for (var k = 0; k < this.accNorm.length - 1; k++) {
+			mean += this.accNorm[k];
+			mean2 += this.accNorm[k] * this.accNorm[k];
+		}
+
+		this.varAcc = ((mean * mean) - mean2) / this.accNorm.length;
+
+		if ((this.varAcc - 0.5) > 0.0) {
+			this.varAcc -= 0.5;
+		}
+
+		if (isNaN(this.varAcc) === 0) {
+			this.filter.setSignalVariance(this.varAcc);
+			this.sensibility = 2.0 * Math.sqrt(this.varAcc) / (9.80665 * 9.80665);
+		}
+		else {
+			this.sensibility = 1.0 / 30.0;
+		}
+	}
+}
+
+export default Pedometer;
+
+/**
+ * Kalman filter for pedometer
+ * @see http://sebastien.menigot.free.fr/index.php?view=article&id=93
+ */
+class KalmanFilter {
+
+	constructor() {
+		this.G  = 1; // filter gain
+		this.Rw = 1; // noise power desirable
+		this.Rv = 10; // noise power estimated
+
+		this.A = 1;
+		this.C = 1;
+		this.B = 0;
+		this.u = 0;
+		this.P = NaN;
+		this.x = NaN; // estimated signal without noise
+		this.y = NaN; //measured
+	}
+
+	filter(measurement) {
+		this.y = measurement;
+
+		if (isNaN(this.x)) {
+			this.x = 1 / this.C * this.y;
+			this.P = 1 / this.C * this.Rv * 1 / this.C;
+		}
+		else {
+
+			// Kalman Filter: Prediction and covariance P
+			this.x = this.A * this.x + this.B * this.u;
+			this.P = this.A * this.P * this.A + this.Rw;
+
+			// Gain
+			this.G = this.P * this.C * 1 / (this.C * this.P * this.C + this.Rv);
+
+			// Correction
+			this.x = this.x + this.G * (this.y - this.C * this.x);
+			this.P = this.P - this.G * this.C * this.P;
+		}
+
+		return this.x;
+	}
+
+	setSignalVariance(variance) {
+		this.Rv = variance;
+	}
+}
