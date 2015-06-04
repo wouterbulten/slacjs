12a257d1bc92f506fc93b8522ba9abe9df2ece05
Wouter Bulten <wouterbulten@mai-ai.org>
2015-06-04 14:38:18 +0200

    Updated the kalman filter

--- src/app/util/kalman.js
+++ src/app/util/kalman.js
@@ -1,12 +1,18 @@
-/**
- * Kalman filter for 1-dimensional data
- */
 class KalmanFilter {
 
+	/**
+	 * Create 1-dimensional kalman filter
+	 * @param  {Number} options.R Process noise
+	 * @param  {Number} options.Q Measurement noise
+	 * @param  {Number} options.A State vector
+	 * @param  {Number} options.B Control vector
+	 * @param  {Number} options.C Measurement vector
+	 * @return {KalmanFilter}
+	 */
 	constructor({R = 1, Q = 1, A = 1, B = 0, C = 1} = {}) {
 
-		this.R = R;
-		this.Q = Q;
+		this.R = R; // noise power desirable
+		this.Q = Q; // noise power estimated
 
 		this.A = A;
 		this.C = C;
@@ -15,6 +21,12 @@ class KalmanFilter {
 		this.x = NaN; // estimated signal without noise
 	}
 
+	/**
+	 * Filter a new value
+	 * @param  {Number} z Measurement
+	 * @param  {Number} u Control
+	 * @return {Number}
+	 */
 	filter(z, u = 0) {
 
 		if (isNaN(this.x)) {
@@ -23,16 +35,16 @@ class KalmanFilter {
 		}
 		else {
 
-			//Compute the predicted mean and covariance
+			//Compute prediction
 			const predX = (this.A * this.x) + (this.B * u);
 			const predCov = ((this.A * this.cov) * this.A) + this.R;
 
-			//Compute kalman gain
-			this.K = predCov * this.C * (1 / ((this.C * predCov * this.C) + this.Q));
+			//Kalman gain
+			const K = predCov * this.C * (1 / ((this.C * predCov * this.C) + this.Q));
 
-			//Perform the correction step
-			this.x = predX + this.K * (z - (this.C * predX));
-			this.cov = this.cov - (this.K * this.C * predCov);
+			//Correction
+			this.x = predX + K * (z - (this.C * predX));
+			this.cov = predCov - (K * this.C * predCov);
 		}
 
 		return this.x;
