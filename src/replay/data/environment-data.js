16d6bf18b275125a962fa4842d2b7c9861bed4ab
Wouter Bulten <wouterbulten@mai-ai.org>
2015-06-16 14:51:47 +0200

    Updated beacon id's

--- src/replay/data/environment-data.js
+++ src/replay/data/environment-data.js
@@ -3,23 +3,23 @@ SlacJsLandmarkPositions = {
         x: 3.76,
         y: 7.87
     },
-    'DoBeacon_upstair2_2015-06-05': {
+    'DoBeacon_upstair2_2015-06-04': {
         x: 7.36,
         y: 7.87
     },
-    'DoBeacon_upstair3_2015-06-06': {
+    'DoBeacon_upstair3_2015-06-04': {
         x: 7.13,
         y: 0
     },
-    'DoBeacon_upstair4_2015-06-07': {
+    'DoBeacon_upstair4_2015-06-04': {
         x: 4.13,
         y: 0
     },
-    'DoBeacon_upstair5_2015-06-08': {
+    'DoBeacon_upstair5_2015-06-04': {
         x: 1.13,
         y: 0
     },
-    'DoBeacon_upstair6_2015-06-08': {
+    'DoBeacon_upstair6_2015-06-04': {
         x: 1.93,
         y: 1.3
     },
