641a3da27e6c188cdc1fd7afa4bab0210531cef1
Wouter Bulten <wouterbulten@mai-ai.org>
2015-06-17 12:40:35 +0200

    New data

---
+++ src/replay/data/environment-data-LowBeacon.js
@@ -0,0 +1,35 @@
+SlacJsLandmarkPositions = {
+    'LowBeacon1_2015-06-16': {
+        x: 3.76,
+        y: 7.87
+    },
+    'LowBeacon2_2015-06-16': {
+        x: 7.36,
+        y: 7.87
+    },
+    'LowBeacon3_2015-06-16': {
+        x: 7.13,
+        y: 0
+    },
+    'LowBeacon4_2015-06-16': {
+        x: 4.13,
+        y: 0
+    },
+    'LowBeacon5_2015-06-16': {
+        x: 1.13,
+        y: 0
+    },
+    'LowBeacon6_2015-06-16': {
+        x: 1.93,
+        y: 1.3
+    },
+    'LowBeacon7_2015-06-16': {
+        x: 3.94,
+        y: 5.42
+    }
+};
+
+SlacJsStartingPosition = {
+    x: 2.46,
+    y: 3.8
+};
