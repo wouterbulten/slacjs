41530a33c55fb612ce664475ea7ff9deb2e5e56e
Wouter Bulten <wouterbulten@mai-ai.org>
2015-06-25 16:32:08 +0200

    Added coordinates of second set of DoBeacons

---
+++ src/replay/data/environment-data-LowBeaconMoving.js
@@ -0,0 +1,35 @@
+SlacJsLandmarkPositions = {
+    'LowBeacon10': {
+        x: 3.76,
+        y: 7.87
+    },
+    'LowBeacon11': {
+        x: 7.36,
+        y: 7.87
+    },
+    'LowBeacon12': {
+        x: 7.13,
+        y: 0
+    },
+    'LowBeacon13': {
+        x: 4.13,
+        y: 0
+    },
+    'LowBeacon14': {
+        x: 1.13,
+        y: 0
+    },
+    'LowBeacon15': {
+        x: 1.93,
+        y: 1.3
+    },
+    'LowBeacon16': {
+        x: 3.94,
+        y: 5.42
+    }
+};
+
+SlacJsStartingPosition = {
+    x: 2.46,
+    y: 3.8
+};
