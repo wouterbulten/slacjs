a9e4a3f405546403989a9ae1847caa1eb5694242
Wouter Bulten <wouterbulten@mai-ai.org>
2015-06-25 16:39:30 +0200

    Renamed first set of DoBeacons

--- src/replay/data/environment-data-LowBeacon.js
+++ src/replay/data/environment-data-LowBeacon.js
@@ -1,29 +1,29 @@
 SlacJsLandmarkPositions = {
-    'LowBeacon1_2015-06-16': {
+    'LowBeacon1': {
         x: 3.76,
         y: 7.87
     },
-    'LowBeacon2_2015-06-16': {
+    'LowBeacon2': {
         x: 7.36,
         y: 7.87
     },
-    'LowBeacon3_2015-06-16': {
+    'LowBeacon3': {
         x: 7.13,
         y: 0
     },
-    'LowBeacon4_2015-06-16': {
+    'LowBeacon4': {
         x: 4.13,
         y: 0
     },
-    'LowBeacon5_2015-06-16': {
+    'LowBeacon5': {
         x: 1.13,
         y: 0
     },
-    'LowBeacon6_2015-06-16': {
+    'LowBeacon6': {
         x: 1.93,
         y: 1.3
     },
-    'LowBeacon7_2015-06-16': {
+    'LowBeacon7': {
         x: 3.94,
         y: 5.42
     }
