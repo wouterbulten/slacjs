0e0c4dafab004f2f8268223c218210c2b3ab5d8a
Wouter Bulten <wouterbulten@mai-ai.org>
2015-07-05 14:12:58 +0200

    Fixed the landmark init test

--- src/tests/landmark-init.js
+++ src/tests/landmark-init.js
@@ -9,8 +9,8 @@ if (window.test === undefined) {
  * @yield {Number}
  */
 function* walkPattern() {
-	const steps = 40;
-	const stepSize = 2;
+	const steps = 16;
+	const stepSize = 5;
 	const quarter = steps / 4;
 
 	for (let i = 0; i < steps; i++) {
@@ -24,7 +24,7 @@ function* walkPattern() {
 			yield {dx: -stepSize, dy: 0};
 		}
 		else if (i < steps) {
-			yield {dx: 0, dy: -stepSize}; 
+			yield {dx: 0, dy: -stepSize};
 		}
 	}
 }
@@ -47,17 +47,27 @@ window.test.landmarkInit = {
 	initialize: function() {
 
 		//Init random landmark
-		this.lX = Math.random() * 30 - 15;
-		this.lY = Math.random() * 30 - 15;
+		this.lX = 13;
+		this.lY = 20;
 
-		this.landmarkSet = new LandmarkInitializationSet();
+		this.landmarkSet = new LandmarkInitializationSet({
+			N: 300,
+			sd: 3,
+			randomN: 0,
+			effectiveParticleThreshold: 200,
+			maxVariance: 4
+		});
+
 		this.canvas = document.getElementById('test-content');
 		this.ctx = this.canvas.getContext('2d');
-		this.ctx.scale(10, 10);
+		this.ctx.scale(20, 20);
 
 		this.userTrace.push({x: this.userX, y: this.userY});
 
 		this.pattern = walkPattern();
+
+		this.ctx.fillStyle = '#ff0000';
+		this.userTrace.forEach((t) => this.ctx.fillRect(this._tx(t.x) - 0.5, this._ty(t.y) - 0.5, 1, 1));
 	},
 
 	iterate: function() {
@@ -72,7 +82,6 @@ window.test.landmarkInit = {
 		const r = Math.sqrt(Math.pow(this.lX - this.userX, 2) + Math.pow(this.lY - this.userY, 2));
 
 		this.landmarkSet.addMeasurement('uid', this.userX, this.userY, r);
-
 		this._draw();
 		console.debug(`True r: ${r}`);
 	},
@@ -87,14 +96,34 @@ window.test.landmarkInit = {
 			const x = this._tx(p.x);
 			const y = this._ty(p.y);
 
-			this.ctx.fillRect(x, y, 0.3, 0.3);
+			this.ctx.fillRect(x, y, 0.5, 0.5);
 		});
 
 		this.ctx.fillStyle = '#ff0000';
-		this.userTrace.forEach((t) => this.ctx.fillRect(this._tx(t.x), this._ty(t.y), 0.5, 0.5));
+		this.ctx.strokeStyle = '#ff0000';
+		this.ctx.lineJoin = 'round';
+		this.ctx.lineWidth = 0.5;
+		this.ctx.beginPath();
+		this.userTrace.forEach(({x, y}, i) => {
 
-		this.ctx.fillStyle = '#00ff00';
-		this.ctx.fillRect(this._tx(this.lX), this._ty(this.lY), 0.5, 0.5);
+			this.ctx.fillRect(this._tx(x) - 0.5, this._ty(y) - 0.5, 1, 1);
+
+			const tX = this._tx(x);
+			const tY = this._ty(y);
+
+			if (i === 0) {
+				this.ctx.moveTo(tX, tY);
+			}
+			else {
+				this.ctx.lineTo(tX, tY);
+			}
+		});
+
+		this.ctx.stroke();
+		this.ctx.closePath();
+
+		this.ctx.fillStyle = '#2B96E3';
+		this.ctx.fillRect(this._tx(this.lX), this._ty(this.lY), 1, 1);
 	},
 
 	_tx: function(x) {
